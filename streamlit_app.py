1bcf4acf38b9ec7138b13f9d10046f97d0a6c1be
vikaswardha949 <vikaswardha249@gmail.com>
2025-03-11 20:17:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,12 @@ st.write(
 cnx =st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True
+st.dataframe(data=my_dataframe, use_container_width=True
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:' 
 ,    my_dataframe
+,    max_selection = 5
 )
 
 if ingredients_list:
@@ -26,6 +27,10 @@ if ingredients_list:
 
     for fruit_chosen in  ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen + ' Nutrition Information ')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         st_df=st.dataframe(data=smoothiefroot_response.json(),  use_container_width=True)
@@ -35,6 +40,7 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
         values('""" + ingredients_string + """')"""
 
 st.write(my_insert_stmt)
+
 
 
 #st.write(my_insert_stmt)
